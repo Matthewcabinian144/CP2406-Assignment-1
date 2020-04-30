150abe377544288e9a9b552c70dce0025276c761
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-04-30 15:41:08 +0800

    Import startup Traffic Simulator

---
+++ Traffic Simulator/src/Bus.java
@@ -0,0 +1,6 @@
+public class Bus extends Vehicle{
+        public Bus(String id){
+            this.id = ("bus_" + id);
+            length = super.getLength() * 3;
+        }
+}
---
+++ Traffic Simulator/src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    String id;
+
+    public Car(String id, Road ){
+
+    }
+
+    public void Car()
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
